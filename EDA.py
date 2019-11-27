2b7d840cb7f76db730a178648dcdde555e33b3c0
Joseorina <orinangare@gmail.com>
2019-11-27 05:08:40 +0300

    EDA

--- EDA.py
+++ EDA.py
@@ -27,3 +27,8 @@ for i in range(1, dataset2.shape[1] + 1):
     f = plt.gca()
     f.axes.get_yaxis().set_visible(False)
     f.set_title(dataset2.columns.values[i - 1])
+    
+    vals = np.size(dataset2.iloc[:, i-1].unique())
+    
+    plt.hist(dataset2.iloc[:, i-1], bins=vals, color='blue')
+plt.tight_layout(rect=[0,0.03,1,0.95])
