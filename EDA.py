3fd95e3404689a172ebc4fdec1a39b9c0c7ddde5
Joseorina <orinangare@gmail.com>
2019-11-27 05:33:37 +0300

    EDA Piechart

--- EDA.py
+++ EDA.py
@@ -40,3 +40,17 @@ dataset2 = dataset[['housing', 'is_referred', 'app_downloaded',
                     'waiting_4_loan','cancelled_loan','received_loan',
                     'rejected_loan','zodiac_sign','left_for_two_month_plus',
                     'left_for_one_month', 'is_referred']]
+
+fig = plt.figure(figsize=(15, 12))
+plt.suptitle('Pie Chart Distributions', fontsize=20)
+for i in range(1, dataset2.shape[1] + 1):
+    plt.subplot(6, 3, i)
+    f = plt.gca()
+    f.axes.get_yaxis().set_visible(False)
+    f.set_title(dataset2.columns.values[i - 1])
+    values = dataset2.iloc[:, i-1].value_counts(normalize=True).values
+    index = dataset2.iloc[:,i-1].value_counts(normalize=True).index
+    
+    plt.pie(values, labels = index, autopct= '%1.1f%%')
+    plt.axis('equal')
+plt.tight_layout(rect=[0,0.03,1,0.95])
